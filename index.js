ba2f7d1c9032083ffef62fbc579f9da711d597ff
Oliver Ziemann <oliver@mordoor.de>
2019-05-28 16:50:59 +0200

    read all scss files in a directory
    extract title, overview and sections

--- index.js
+++ index.js
@@ -1,18 +1,53 @@
 const FileLoader = require("./lib/FileLoader.js");
+const loader = new FileLoader();
+const fs = require("fs");
+const path = require("path");
 
-let articles = [];
 main(process.argv);
 
 function main(argv) {
-    //1. Load Files
-    let loader = new FileLoader();
-    if(argv[2] !== null) {
-       let article = loader.readFileFromURL(argv[2]);
-       articles.push(article);
-       console.log("Artikel: " + articles);
-    } else {
-        "No readable File! Please define a path in the CommandLine."
-    }
-    //2. create Articles
-    //3. write HTML
+    console.log(argv);
+    let path = argv[2]
+    let filesUrls = getFilesFromDirectory(path);
+    let articles = extractArticlesFromFiles(filesUrls);
+    let snippets = renderArticlesToHtml(articles);
+
+    writeIndexHtml(snippets);
 }
+
+function getFilesFromDirectory(directory) {
+    let scssFiles = [];
+    console.log("---------------------");
+    console.log("FILES:");
+    console.log("---------------------");
+    fs.readdirSync(directory, "utf8").forEach(file => {
+        if (path.extname(file) === ".scss") {
+            console.log(file);
+            scssFiles.push(file);
+        }
+    });
+    console.log("---------------------");
+    console.log("---------------------");
+
+    return scssFiles;
+}
+
+function extractArticlesFromFiles(filesUrls) {
+
+    let articles = [];
+    filesUrls.forEach((fileUrl) => {
+        let article = loader.readFileFromURL(fileUrl);
+        articles.push(article);
+        console.log("---------------------");
+        console.log("ARTICLES:");
+        console.log("---------------------");
+        console.log(articles);
+        console.log("---------------------");
+    });
+
+    return articles;
+}
+
+function renderArticlesToHtml() { }
+
+function writeIndexHtml() { }
---
+++ index.tpl
@@ -0,0 +1,19 @@
+<html>
+
+<head>
+<link href=CSS_PLACEHOLDER>
+</head>
+
+<body>
+
+<!-- navbar -->
+<nav>
+..
+</nav>
+
+<!-- snippets -->
+SNIPPET_PLACEHOLDER
+
+</body>
+
+</html>
