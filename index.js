1415131181824965a4289226f73211bf0ee5c4cc
Oliver Ziemann <oliver@mordoor.de>
2019-05-29 12:10:18 +0200

    format console logs

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ main(process.argv);
 
 function main(argv) {
     let path = argv[2]
+    console.log("---------------------");
     console.log("DIRECTORY TO LOOK FOR FILES: " + path);
     let filesUrls = getFilesFromDirectory(path);
     let articles = extractArticlesFromFiles(filesUrls);
@@ -15,13 +16,14 @@ function main(argv) {
     let template = fillTemplateWithHTML(path, snippets);
 
     writeIndexHtml(template);
+    console.log("FINISHED! HAVE FUN WITH YOUR STYLEGUIDE :D");
+    console.log("---------------------");
 }
 
 function getFilesFromDirectory(directory) {
     let scssFiles = [];
     console.log("---------------------");
-    console.log("FILES:");
-    console.log("---------------------");
+    console.log("FILES FOUND:");
     fs.readdirSync(directory, "utf8").forEach(file => {
         if (path.extname(file) === ".scss") {
             console.log(file);
@@ -54,30 +56,30 @@ function renderArticlesToHtml() {
     console.log("---------------------");
     articles.map((article) => {
         console.log("RENDER ARTICLE: " + article.title);
-        let title = "<h1 id=\"" + article.title + "\">" + article.title + "</h1>";
-        let overview = "<p>" + article.overview + "</p>";
+        let title = "<h1 id=\"" + article.title + "\">" + article.title + "</h1>\n";
+        let overview = "<p>" + article.overview + "</p>\n";
         let artHTMLString = "<article>" + title + overview;
         article.sections.map((section) => {
-            let sec = "<section><div>";
+            let sec = "<section><div>\n";
             let example = section[0].example;
-            let description = "<p>" + section[0].description + "</p>";
-            let html = section[0].html;
+            let description = "<p>" + section[0].description + "</p>\n";
+            let html = "<figure>\n<pre>\n<code>\n" + section[0].html + "</code>\n</pre>\n</figure>\n";
 
             sec = sec + example + description + html;
             sec = sec + "</div></section>";
             artHTMLString = artHTMLString + sec;
         })
         artHTMLString = artHTMLString + "</article>";
-        console.log("---------------------");
-        console.log(artHTMLString);
-        console.log("---------------------");
         articlesAsString = articlesAsString + artHTMLString;
     })
+    console.log("---------------------");
     articlesAsString = articlesAsString + "</div>";
     return articlesAsString;
 }
 
 function fillTemplateWithHTML(directory, snippets) {
+    console.log("FILL TEMPLATE WITH HTML");
+    console.log("---------------------");
     let htmlTemplate = null;
     fs.readdirSync(directory, "utf8").forEach(file => {
         if (path.basename(file) === "template.html") {
@@ -100,4 +102,5 @@ function fillTemplateWithHTML(directory, snippets) {
 function writeIndexHtml(htmlContent) { 
     fs.writeFileSync('./index.html', htmlContent);
     console.log("WRITE FILE index.html");
+    console.log("---------------------");
 }
