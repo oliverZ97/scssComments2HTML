c7ee2c3cd4adb6a9c9d65ddac0540f8565ed99e6
Oliver Ziemann <oliver@mordoor.de>
2019-06-05 09:32:25 +0200

    css, js bundle get minified

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const path = require('path');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const devMode = process.env.NODE_ENV !== 'production';
+const TerserJSPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const webpack = require('webpack');
 
 console.log('im running in DevMode: ' + devMode);
@@ -77,5 +79,8 @@ module.exports = {
     ],
     node: {
         fs: 'empty'
+    },
+    optimization: {
+        minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
     }
 };
