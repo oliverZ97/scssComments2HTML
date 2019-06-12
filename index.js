513d0ca838253f2afefe8120473392fc539a3c49
Oliver Ziemann <oliver@mordoor.de>
2019-06-12 09:59:05 +0200

    style copy button

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ function renderArticlesToHtml() {
                 sec = "<section class=\"lsg_section-ds\"><div>\n";
                 let example = section[0].example;
                 let description = "<p>" + section[0].description + "</p>\n";
-                let copyBtn = "<button class=\"clipboard\" data-clipboard-target=\"" + trigger + "\">copy</button>\n";
+                let copyBtn = "<button class=\"clipboard lsg_button\" data-clipboard-target=\"" + trigger + "\">copy</button>\n";
                 let mask = hljs.highlight('javascript', section[0].html).value;
                 let html = "<figure>\n<pre>\n<code id=\"" + id + "\" >\n" + mask + "</code>\n</pre>\n</figure>\n";
 
@@ -105,11 +105,11 @@ function renderArticlesToHtml() {
                 sec = "<section class=\"lsg_section\"><div>\n";
                 let example = section[0].example;
                 let description = "<p>" + section[0].description + "</p>\n";
-                let copyBtn = "<button class=\"clipboard\" data-clipboard-target=\"" + trigger + "\">copy</button>\n";
+                let copyBtn = "<button class=\"clipboard lsg_button\" data-clipboard-target=\"" + trigger + "\">copy</button>\n";
                 let mask = hljs.highlight('javascript', section[0].html).value;
                 let html = "<figure id=\"" + id + "\" >\n<pre>\n<code >\n" + mask + "</code>\n</pre>\n</figure>\n";
 
-                sec = sec + "<div class=\"lsg_description\">" + description +  copyBtn + "</div>\n<div class=\"lsg_snippet\">" + "<div class=\"lsg_example\">" + example + "</div>\n<div class=\"lsg_snip\">" + html + "</div>\n</div>";
+                sec = sec + "<div class=\"lsg_section_header\">" + description +  copyBtn + "</div>\n<div class=\"lsg_snippet\">" + "<div class=\"lsg_example\">" + example + "</div>\n<div class=\"lsg_snip\">" + html + "</div>\n</div>";
                 sec = sec + "</div></section>";
             }
             artHTMLString = artHTMLString + sec;
